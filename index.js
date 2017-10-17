a1dec149ed7e403dd93d01d006720eec9e33ff50
Tony <1414927+zfbTony@users.noreply.github.com>
2017-10-17 13:23:01 -0400

    cleaned up a little bit and added a few replies

--- index.js
+++ index.js
@@ -4,57 +4,74 @@ const util = require('util');
 
 const vside = new Discord.Client({
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['TYPING_START', 'CHANNEL_PINS_UPDATE', 'USER_NOTE_UPDATE']
 });
 const bob = new Discord.Client({ //NV Bot
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_DELETE_BULK', 
+    'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE', 'MESSAGE_REACTION_REMOVE_ALL', 
+    'USER_NOTE_UPDATE', 'VOICE_STATE_UPDATE', 'TYPING_START', 
+    'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
 });
 const mann = new Discord.Client({ //RJ Bot
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_DELETE_BULK', 
+    'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE', 'MESSAGE_REACTION_REMOVE_ALL', 
+    'USER_NOTE_UPDATE', 'VOICE_STATE_UPDATE', 'TYPING_START', 
+    'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
 });
 const miah = new Discord.Client({ //LGA Bot
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_DELETE_BULK', 
+    'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE', 'MESSAGE_REACTION_REMOVE_ALL', 
+    'USER_NOTE_UPDATE', 'VOICE_STATE_UPDATE', 'TYPING_START', 
+    'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
 });
 const nene = new Discord.Client({ //Gateway Bot
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_DELETE_BULK', 
+    'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE', 'MESSAGE_REACTION_REMOVE_ALL', 
+    'USER_NOTE_UPDATE', 'VOICE_STATE_UPDATE', 'TYPING_START', 
+    'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
 });
 const susp = new Discord.Client({ //Random servers
     disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+    disabledEvents: ['CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_DELETE_BULK', 
+    'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE', 'MESSAGE_REACTION_REMOVE_ALL', 
+    'USER_NOTE_UPDATE', 'VOICE_STATE_UPDATE', 'TYPING_START', 
+    'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE']
 });
+
+
 
 vside.on("ready", () => {
     vside.user.setGame('vSide');
-    console.log(`vSide is online!\n${vside.users.size} users found.`);
+    console.log(`vSide bots are online!\n${vside.users.size} users found.`);
 });
 bob.on("ready", () => {
     bob.user.setGame('vSide');
-    console.log(`Bob is online.`);
+    //console.log(`Bob is online.`);
 });
 mann.on("ready", () => {
     mann.user.setGame('vSide');
-    console.log(`MannequinOfTheNight is online.`);
+    //console.log(`MannequinOfTheNight is online.`);
 });
 miah.on("ready", () => {
     miah.user.setGame('vSide');
-    console.log(`Miah is online.`);
+    //console.log(`Miah is online.`);
 });
 nene.on("ready", () => {
     nene.user.setGame('vSide');
-    console.log(`Nene is online.`);
+    //console.log(`Nene is online.`);
 });
 susp.on("ready", () => {
     susp.user.setGame('vSide');
-    console.log(`SuspiciousMan is online.`);
+    //console.log(`SuspiciousMan is online.`);
 });
 
 
 vside.on("message", async message => { 
-    if(message.author.vside || message.system) return;
+    if(message.author.bot || message.system) return;
     if(message.channel.type === 'dm') { return; } 
 
     if (message.content.indexOf(config.prefix) === 0) {
@@ -62,25 +79,47 @@ vside.on("message", async message => {
         let cmd = args[0].toLowerCase();
         args.shift();
 
-        if (cmd === 'hi' || cmd === 'hello') {
+        if (cmd === 'hi') {
             message.channel.send(`Hi there ${message.author.toString()}`);
             return; 
         }
+
         else if (cmd === "eval" && message.author.id === config.owner){
             const code = args.join(" ");
             return evalCmd(message, code);
         }
-        else { return; }
+        
+        return;
     } else if (message.content.indexOf("<@"+vside.user.id) === 0 || message.content.indexOf("<@!"+vside.user.id) === 0) {
-        return message.channel.send(`Use \`${config.prefix}\` to interact with me.`);
+        return;// message.channel.send(`Use \`${config.prefix}\` to interact with me.`);
     }
     return;
 });
 
+susp.on("message", async message => { 
+    if (message.author.bot || message.system) return;
+    let msgs = ["My clothes tell secrets","find the key...(his eyes dart side to side looking for something)", "I've got Nothing to Hide"];
+    if (message.channel.type === 'dm') { 
+        return message.channel.send(msgs[Math.floor(Math.random()*msgs.length)]);
+    } 
 
-bob.on("message", (message) => require("./events/bob.js")(bob, message))
+    if (message.content.indexOf("<@"+susp.user.id) === 0 || message.content.indexOf("<@!"+susp.user.id) === 0) {
+        return message.channel.send(msgs[Math.floor(Math.random()*msgs.length)]);
+    }
+    return;
+});
 
+bob.on("message", async message => { 
+    if (message.author.bot || message.system) return;
+    if (message.channel.type === 'dm') { 
+        return message.channel.send(`Hey, good to see you again. No trivia today. Try tomorrow.`);
+    } //Hey, good to see you again. You already answered this day's trivia. Try tomorrow. 
 
+    if (message.content.indexOf("<@"+bob.user.id) === 0 || message.content.indexOf("<@!"+bob.user.id) === 0) {
+        return message.channel.send(`Hey, good to see you again. No trivia today. Try tomorrow.`);
+    }
+    return;
+});
 
 
 
